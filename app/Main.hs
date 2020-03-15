29df171b48dc749cb507af410d10215d31d94536
Igor Beschastnov <beschastnovigor@gmail.com>
2020-03-15 18:08:30 +0300

    init llvm

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,36 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-main :: IO ()
-main = putStr "Hello world!"
+import Codegen
+import JIT
+import qualified LLVM.AST as AST
+import qualified LLVM.AST.Constant as C
+import qualified LLVM.AST.Float as F
+
+{-
+
+; ModuleID = 'my cool jit'
+
+define double @main() {
+entry:
+  %1 = fadd double 1.000000e+01, 2.000000e+01
+  ret double %1
+}
+
+-}
+
+initModule :: AST.Module
+initModule = emptyModule "my cool jit"
+
+logic :: LLVM ()
+logic = do
+  define double "main" [] $ \ptrToMain -> do
+    let a = cons $ C.Float (F.Double 10)
+    let b = cons $ C.Float (F.Double 20)
+    res <- fadd a b
+    ret res
+
+main :: IO AST.Module
+main = do
+  let ast = runLLVM initModule logic
+  rc <- runJIT ast
+  return ast

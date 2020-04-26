38931d01f20d70738feb188ec6fb516ea6979aa1
Igor Beschastnov <beschastnovigor@gmail.com>
2020-04-26 16:36:01 +0300

    Working on emiting correct IR.

--- app/Builder.hs
+++ app/Builder.hs
@@ -50,19 +50,25 @@ load :: MonadIRBuilder m => Operand -> m Operand
 load pointer = I.load pointer alignment
 
 store :: MonadIRBuilder m => Operand -> Operand -> m ()
-store value pointer = I.store pointer alignment value
+store pointer value = I.store pointer alignment value
 
 saveInt :: MonadIRBuilder m => Integer -> m Operand
 saveInt value = do
   pointer <- allocateInt
-  store (int32 value) pointer
+  store pointer (int32 value)
   return pointer
 
 refName :: String -> A.Name
 refName name = Name (toShort' $  name ++ "_0")
 
+reference :: AST.Type -> String -> Operand
+reference type_ name = LocalReference type_ (refName name)
+
 referenceInt :: String -> Operand
-referenceInt name = LocalReference i32 (refName name)
+referenceInt name = reference i32 name
+
+referenceIntPointer :: String -> Operand
+referenceIntPointer name = reference integerPointer name
 
 typeMap = Map.fromList [(IntType, i32)]
 
@@ -70,25 +76,51 @@ argDef (Def defType name) = (typeMap ! defType, ParameterName $ toShort' name)
 
 extractDefs :: MonadIRBuilder m => [Expr] -> m ()
 extractDefs (expr:exprs) = do
-  fromMaybe (pure ()) $ extractDef expr
+  extractDef expr
   extractDefs exprs
 extractDefs [] = pure ()
 
-extractDef :: MonadIRBuilder m => Expr -> Maybe (m ())
+extractDef :: MonadIRBuilder m => Expr -> m ()
 extractDef (BinaryOp "=" maybeDef _) = case maybeDef of
-  Def defType defName -> Just $ do
+  Def defType defName -> do
     allocateInt `named` toShort' defName
     pure ()
-  _ -> Nothing
-extractDef _ = Nothing
-  
+  _ -> pure ()
+extractDef _ = pure ()
+
+emitAll :: MonadIRBuilder m => [Expr] -> m ()
+emitAll (expr:exprs) = do
+  emit expr
+  emitAll exprs
+emitAll [] = pure ()
+
+emit :: MonadIRBuilder m => Expr -> m ()
+emitInner :: MonadIRBuilder m => Expr -> m Operand
+
+-- Binary Op, UnaryOp
+emitInner _ = error "Impossible inner expression (error messages are WIP)"
+
+emit (BinaryOp "=" dest object) = 
+  do
+    value <- case object of
+      Int i -> pure (int32 i)
+      Var v -> load (referenceIntPointer v)
+      _ -> emitInner object 
+    store (referenceIntPointer name) value
+  where
+    name = case dest of 
+      Def _ n -> n
+      Var n -> n
+
+emit _ = pure ()
+
 funcBodyBuilder :: MonadIRBuilder m => [Expr] -> ([Operand] -> m ())
 funcBodyBuilder bodyTokens = func
-  where 
+  where
     func argOperands = do
+      -- Steps of codegen
       extractDefs bodyTokens
-      value <- add (integerConstant 1) (integerConstant 2)
-      ret value
+      emitAll bodyTokens
 
 functionAST (Syntax.Function retType name args body) = 
   function (Name $ toShort' name) arguments (typeMap ! retType) funcBody

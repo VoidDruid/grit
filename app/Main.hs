b919ecb1a69bdd28e60a9dac039fca919c85647a
Igor Beschastnov <beschastnovigor@gmail.com>
2020-04-28 16:00:56 +0300

    Add cli options.

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Either
+import Data.List (intersect)
 import qualified Data.Text.Lazy.IO as TLIO
 
 import System.IO
 import System.Environment
+import System.Exit
 import System.Console.Haskeline
 
 import qualified LLVM.AST as AST
@@ -20,16 +23,26 @@ processFile fname = do
   code <- readFile fname
   let parseResult = P.parseTopLevel code
   let buildResult = case parseResult of
-        Left err -> "Error"
-        Right result -> ppllvm $ B.buildAST result
-  putStrLn "TOKENS \n"
-  print parseResult
-  putStrLn "\nLLVM IR \n"
-  TLIO.putStrLn buildResult
+        Left err -> Nothing
+        Right result -> Just $ B.buildAST result
+  return (parseResult, buildResult)
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    []      -> putStrLn "Provide file name!"
-    [fname] -> processFile fname >> return ()
+main =
+  do
+    args <- getArgs
+    case args of
+      []      -> putStrLn "Provide file name!"
+      (reverse -> (fname:args)) -> do
+        (mTokens, mIR) <- processFile fname
+        case mTokens of
+          Left err -> print err
+          Right tokens -> actionFor ["--debug", "-d"] (print tokens >> putStrLn "")
+        case mIR of
+          Nothing -> pure ()
+          Just ir -> actionFor ["--emit", "-e"] (TLIO.putStrLn $ ppllvm ir)
+        return ()
+        where
+          actionFor key action = if not (null (key `intersect` args))
+            then action
+            else pure ()
